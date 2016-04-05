28b59879a0c006d7461a33824befe2b6bc2c0bf2
girishjakkam <girish.jakkam@gmail.com>
2016-04-05 20:08:12 +0530

    Update q1.sh

--- q1.sh
+++ q1.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 #How to write shell script that will add two nos, which are supplied as command line argument, and if this two nos are not given show error and its usage
 
-#!/bin/bash
+
 if [ $# -ne 2 ]
 then 
 echo "Input arguments are wrong" 
