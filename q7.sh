7fd5caab5189aa06489d4e4aff8c17f6dd30eeb9
girishjakkam <girish.jakkam@gmail.com>
2016-04-05 20:12:38 +0530

    Update q7.sh

--- q7.sh
+++ q7.sh
@@ -1,6 +1,8 @@
+#! /bin/bash
+
 #Write script to print given numbers sum of all digit, For eg. If no is 123 it's sum of all digit will be 1+2+3 = 6.
 
-#! /bin/bash
+
 z=`expr $1 % 100 - $1 % 10 `
 y=`expr $z / 10 `
 echo `expr $1 / 100 + $y + $1 % 10 `
