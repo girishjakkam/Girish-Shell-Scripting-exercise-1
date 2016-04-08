04babff4b6b20faf3d4d8851d11e13f3a08cef89
girishjakkam <girish.jakkam@gmail.com>
2016-04-08 13:02:01 +0530

    Added files via upload

--- pro.sh
+++ pro.sh
@@ -1,10 +1,20 @@
 #! /bin/bash
 
 ps >tm
-ps -a>tm1
+ps -ag>tm1
 
 sort -r tm1
 tail tm1
 
 grep "tty3" tm1
 
+pstree
+top
+
+read proceskilled
+
+kill $proceskilled
+
+read PIDwatch
+
+ps as | grep $PIDwatch
